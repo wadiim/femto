aec6bc4a27d6ee84f80bb4d22ef5b4676c9e6566
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2019-02-22 10:35:37 +0100

    Reword term.h to contain fewer blank lines

--- include/term.h
+++ include/term.h
@@ -5,22 +5,15 @@
 #include <stdbool.h>
 
 void term_init(void);
-
 void get_window_size(size_t *x, size_t *y);
-
 void clear_line(void);
 void clear_screen(void);
-
 void set_title(const char *title);
-
 void enable_raw_mode(void);
 void disable_raw_mode(void);
-
 void get_buffer_size(size_t *x, size_t *y);
-
 void switch_to_normal_buffer(void);
 void switch_to_alternate_buffer(void);
-
 void hide_cursor(void);
 void show_cursor(void);
 void set_cursor_position(size_t x, size_t y);
